fcda83a4c26419951dd4f704aba8de7ec352e03b
Harsh-Gehlot <harshgehlot13ime@gmail.com>
2022-07-21 22:55:03 +0530

    added shape coloum to df

--- preprocess.py
+++ preprocess.py
@@ -1,22 +1,26 @@
 import os
 import pandas as pd
-
-df1 = pd.read_csv('labelSet\Eocanthecona_Bug_A.csv')
-df2 = pd.read_csv('labelSet\Larva_Spodoptera_D.csv')
-df3 = pd.read_csv('labelSet\Red_Hairy_Catterpillar_C.csv')
-df4 = pd.read_csv('labelSet\Tobacco_Caterpillar_B.csv')
+import cv2
 
 
-# def getPath(x):
-#     return os.path.join(x["lable"],x["image"])
+def img_shape(path):
+    img = cv2.imread(os.path.join("dataset", path))
+    return img.shape
 
-df1['path'] = df1['label'] + "/" + df1['image']
-df2['path'] = df2['label'] + "/" + df2['image']
-df3['path'] = df3['label'] + "/" + df3['image']
-df4['path'] = df4['label'] + "/" + df4['image']
 
-# print(df1.head())
-frames = [df1,df2,df3,df4]
+df1 = pd.read_csv('dataset\Eocanthecona_Bug_A.csv')
+df2 = pd.read_csv('dataset\Larva_Spodoptera_D.csv')
+df3 = pd.read_csv('dataset\Red_Hairy_Catterpillar_C.csv')
+df4 = pd.read_csv('dataset\Tobacco_Caterpillar_B.csv')
+
+df1['path'] = df1['label'] + "\\" + df1['image']
+df2['path'] = df2['label'] + "\\" + df2['image']
+df3['path'] = df3['label'] + "\\" + df3['image']
+df4['path'] = df4['label'] + "\\" + df4['image']
+
+
+frames = [df1, df2, df3, df4]
+
 df = pd.concat(frames)
-
+df['shape'] = df.apply(lambda x: img_shape(x), axis=1)
 df.to_csv("MetaData.csv")
